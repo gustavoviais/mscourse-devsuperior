a0535f8b5ea860c73758bf6ab202770036ff39a0
Gustavo Viais <gustavoviais@gmail.com>
2021-06-07 10:21:03 -0300

    Aula1

---
+++ hr-worker/src/main/java/com/devsuperior/MsCouseDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCouseDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCouseDevSuperiorApplication.class, args);
+	}
+
+}
